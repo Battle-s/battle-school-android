45d41fa51b7af4459609c26897a1e4100727f42d
Zzu-h <juho7785@naver.com>
2022-07-17 22:16:16 +0900

    chore: 프로젝트 구조 설계 - layout 파일 및 필수 activity 생성 - 프로젝트 구조 설계

---
+++ app/src/main/java/com/umc/battles/ui/auth/bottomsheet/SignUpFragment.kt
@@ -0,0 +1,9 @@
+package com.umc.battles.ui.auth.bottomsheet
+
+import com.umc.battles.databinding.FragmentSignUpBinding
+import com.umc.battles.ui.BaseFragment
+
+class SignUpFragment: BaseFragment<FragmentSignUpBinding>(FragmentSignUpBinding::inflate){
+
+    override fun initAfterBinding() {}
+}
