283203dc575f270621e641257a8e531f367fb01d
Zzu-h <juho7785@naver.com>
2022-07-31 20:41:31 +0900

    ui: 회원가입 ui 이동 구현

--- app/src/main/java/com/umc/battles/ui/auth/bottomsheet/SignUpFragment.kt
+++ app/src/main/java/com/umc/battles/ui/auth/bottomsheet/SignUpFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.umc.battles.databinding.FragmentSignUpBinding
+import com.umc.battles.ui.BaseActivity
+import com.umc.battles.ui.auth.bottomsheet.adapter.SignUpContentVPAdapter
+import com.umc.battles.ui.main.MainActivity
+
 
 class SignUpFragment : BottomSheetDialogFragment() {
 
@@ -17,11 +22,48 @@ class SignUpFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSignUpBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
 
+    private fun initView() {
+        val adapter = SignUpContentVPAdapter(this)
+            .apply { onClickButton = this@SignUpFragment::onClickButton }
+        binding.vpSignupContent.adapter = adapter
+
+        binding.vpSignupContent.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+
+                val tag = adapter.getFragmentTag(position)
+
+                binding.tvSignupTitle.text =
+                    if (tag == SignUpSchoolInfoFragment.TAG) "학교정보" else "사용자 정보"
+                binding.btnBack.setOnClickListener {
+                    if (tag == SignUpSchoolInfoFragment.TAG) {
+                    } else {
+                        val size = binding.vpSignupContent.adapter!!.itemCount
+                        val currentIdx = binding.vpSignupContent.currentItem
+                        binding.vpSignupContent.currentItem = (currentIdx - 1)
+                    }
+                }
+            }
+        })
+    }
+
+    private fun onClickButton(tag: String) {
+        if (tag == SignUpSchoolInfoFragment.TAG) {
+            val size = binding.vpSignupContent.adapter!!.itemCount
+            val currentIdx = binding.vpSignupContent.currentItem
+            binding.vpSignupContent.currentItem = (currentIdx + 1)
+        } else {
+            (requireActivity() as BaseActivity<*>).startActivityWithClear(MainActivity::class.java)
+        }
     }
 }
