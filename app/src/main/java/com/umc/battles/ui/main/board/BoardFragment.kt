e4588707ae32a8f9b55455ca52081bb6094e8ae2
Zzu-h <juho7785@naver.com>
2022-08-07 23:31:13 +0900

    ui: 게시판 메인 화면 구현
    - 게시판 메인 화면 구현

--- app/src/main/java/com/umc/battles/ui/main/board/BoardFragment.kt
+++ app/src/main/java/com/umc/battles/ui/main/board/BoardFragment.kt
@@ -6,6 +6,12 @@ import com.umc.battles.ui.BaseFragment
 class BoardFragment : BaseFragment<FragmentBoardBinding>(FragmentBoardBinding::inflate) {
 
     override fun initAfterBinding() {
+        initView()
+    }
 
+    private fun initView() {
+        binding.layoutAllBoard.tvTitle.text = "전체게시판"
+        binding.layoutSchoolBoard.tvTitle.text = "인하대게시판"
+        binding.layoutHotBoard.tvTitle.text = "HOT 게시물"
     }
 }
