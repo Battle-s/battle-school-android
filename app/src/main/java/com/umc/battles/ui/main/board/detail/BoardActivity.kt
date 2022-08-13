f3903792c286975e10c2812783c73028feeb19d6
Zzu-h <juho7785@naver.com>
2022-08-13 20:07:05 +0900

    ui: 게시판 상세 화면 구현
    - 게시판 상세 화면 구현

---
+++ app/src/main/java/com/umc/battles/ui/main/board/detail/BoardActivity.kt
@@ -0,0 +1,29 @@
+package com.umc.battles.ui.main.board.detail
+
+import com.umc.battles.databinding.ActivityBoardBinding
+import com.umc.battles.ui.BaseActivity
+import com.umc.battles.ui.main.board.BoardFragment.Companion.board_all
+import com.umc.battles.ui.main.board.BoardFragment.Companion.board_hot
+import com.umc.battles.ui.main.board.BoardFragment.Companion.board_school
+import com.umc.battles.ui.main.board.BoardFragment.Companion.board_tag
+
+class BoardActivity : BaseActivity<ActivityBoardBinding>(ActivityBoardBinding::inflate) {
+
+    private val tag: String by lazy { intent.getStringExtra(board_tag) ?: "Invalid Tag" }
+
+    override fun initAfterBinding() {
+        initView()
+    }
+
+    private fun initView() {
+        binding.tvBoardTitle.text = when (tag) {
+            board_all -> "전체게시판"
+            board_school -> "인하대게시판"
+            board_hot -> "HOT 게시물"
+            else -> {
+                finish()
+                ""
+            }
+        }
+    }
+}
