d9ea097e832cb300ba1b5f3c01a22d76446d66d4
Zzu-h <juho7785@naver.com>
2022-08-13 23:53:48 +0900

    ui: battle 화면의 실 내용 구성
    - 실제 fragment 내용을 추가함
    - tablayout 동작 등

---
+++ app/src/main/java/com/umc/battles/ui/main/battle/battle/BattleJoinFragment.kt
@@ -0,0 +1,12 @@
+package com.umc.battles.ui.main.battle.battle
+
+import com.umc.battles.databinding.FragmentBattleBinding
+import com.umc.battles.databinding.FragmentBattleJoinBinding
+import com.umc.battles.ui.BaseFragment
+
+class BattleJoinFragment : BaseFragment<FragmentBattleJoinBinding>(FragmentBattleJoinBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+    }
+}
---
+++ app/src/main/java/com/umc/battles/ui/main/battle/club/ClubJoinFragment.kt
@@ -0,0 +1,12 @@
+package com.umc.battles.ui.main.battle.club
+
+import com.umc.battles.databinding.FragmentBattleBinding
+import com.umc.battles.databinding.FragmentClubJoinBinding
+import com.umc.battles.ui.BaseFragment
+
+class ClubJoinFragment : BaseFragment<FragmentClubJoinBinding>(FragmentClubJoinBinding::inflate) {
+
+    override fun initAfterBinding() {
+
+    }
+}
