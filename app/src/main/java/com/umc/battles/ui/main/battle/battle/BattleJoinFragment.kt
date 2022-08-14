a6e17ed3d26dbbe0fcc6e8fcb804f3580c599a6c
Zzu-h <juho7785@naver.com>
2022-08-14 20:20:27 +0900

    ui: battle 메뉴의 club/battle 화면 구현
    - club : recycler view 및 spinner ui 구현
    - battle : recycler view, spinner 및 검색 ui 구현

--- app/src/main/java/com/umc/battles/ui/main/battle/battle/BattleJoinFragment.kt
+++ app/src/main/java/com/umc/battles/ui/main/battle/battle/BattleJoinFragment.kt
@@ -1,10 +1,10 @@
 package com.umc.battles.ui.main.battle.battle
 
-import com.umc.battles.databinding.FragmentBattleBinding
 import com.umc.battles.databinding.FragmentBattleJoinBinding
 import com.umc.battles.ui.BaseFragment
 
-class BattleJoinFragment : BaseFragment<FragmentBattleJoinBinding>(FragmentBattleJoinBinding::inflate) {
+class BattleJoinFragment :
+    BaseFragment<FragmentBattleJoinBinding>(FragmentBattleJoinBinding::inflate) {
 
     override fun initAfterBinding() {
 
--- app/src/main/java/com/umc/battles/ui/main/battle/club/ClubJoinFragment.kt
+++ app/src/main/java/com/umc/battles/ui/main/battle/club/ClubJoinFragment.kt
@@ -1,6 +1,5 @@
 package com.umc.battles.ui.main.battle.club
 
-import com.umc.battles.databinding.FragmentBattleBinding
 import com.umc.battles.databinding.FragmentClubJoinBinding
 import com.umc.battles.ui.BaseFragment
 
