c5a078f41957fca27b9e96abe18ebbb6315b2b0c
Zzu-h <juho7785@naver.com>
2022-08-21 21:48:22 +0900

    feat: 작업 위치 변경을 위한 원격 저장소 push

--- app/src/main/java/com/umc/battles/ui/main/chart/ChartFragment.kt
+++ app/src/main/java/com/umc/battles/ui/main/chart/ChartFragment.kt
@@ -4,6 +4,21 @@ import com.umc.battles.databinding.FragmentChartBinding
 import com.umc.battles.ui.BaseFragment
 
 class ChartFragment : BaseFragment<FragmentChartBinding>(FragmentChartBinding::inflate) {
+
+    private val categoryTagList = listOf("운동", "e-sports", "보드게임", "학술")
+
     override fun initAfterBinding() {
+        initView()
+    }
+
+    private fun initView() {
+        initTabLayout()
+    }
+
+    private fun initTabLayout() {
+
+        with(binding.tlCategory) {
+            categoryTagList.forEach { this.addTab(this.newTab().setText(it)) }
+        }
     }
 }
